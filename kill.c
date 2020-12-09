61253fe0be1dc9d46f69453ee41d8e225fb41d50
yufengzhe66 <17808459+yufengzhe66@users.noreply.github.com>
2020-12-09 16:06:43 +0800

    Add files via upload

--- kill.c
+++ kill.c
@@ -11,14 +11,17 @@ int main()
   pid_t pid = fork();
   if(pid > 0)
    {
+     while(1)
+     {
      printf("parent, pid = %d\n", getpid());
-     while(1);
+     sleep(1);
+     }
    }
    else if(pid == 0)
    {
      printf("child pid = %d, ppid = %d\n",getpid(), getppid());
-     sleep(2);
-     kill(getppid(), SIGKILL);
+     sleep(10);
+     kill(0, SIGKILL);
    }
    return 0;
 }
